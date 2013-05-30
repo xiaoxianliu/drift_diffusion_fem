0e5fe65286b6604805b867446a16a5ed3ebbdc96
xiaoxian <liuxiaox@sas.upenn.edu>
2013-05-30 13:37:26 -0400

    finish modifying "write_gnuplot.cpp" and "write_polyfile.cpp" to vector-based MeshData

--- triangle/write_gnuplot.cpp
+++ triangle/write_gnuplot.cpp
@@ -6,44 +6,44 @@
 using namespace std;
 
 
-void WriteGNUplot(MeshData &mesh, const string& polyname){
+void WriteGNUplot(MeshData &mesh, string filename){
 
 	/* Write data file */
 	ofstream output;
-	string filename = polyname + "_mesh.dat";
-	output.open(filename.c_str());
+	string meshdat_filename = filename + "_mesh.dat";
+	output.open(meshdat_filename.c_str());
 
 	if (!output.is_open())
 		{cout << "Fail to open "<<filename<<endl; exit(1);}
 
 	for (int i=0; i<mesh.num_elements; i++)
-	{	int v0 = mesh.elements[3*i];
-		int v1 = mesh.elements[3*i+1];
-		int v2 = mesh.elements[3*i+2];
+	{	int v0 = mesh.elements[i][0];
+		int v1 = mesh.elements[i][1];
+		int v2 = mesh.elements[i][2];
 
-		output<<mesh.nodes[2*v0] << "\t" << mesh.nodes[2*v0+1] << "\n";
-		output<<mesh.nodes[2*v1] << "\t" << mesh.nodes[2*v1+1] << "\n";
-		output<<mesh.nodes[2*v2] << "\t" << mesh.nodes[2*v2+1] << "\n";
-		output<<mesh.nodes[2*v0] << "\t" << mesh.nodes[2*v0+1] << "\n\n";
+		output<<mesh.nodes[v0][0] << "\t" << mesh.nodes[v0][1] << "\n";
+		output<<mesh.nodes[v1][0] << "\t" << mesh.nodes[v1][1] << "\n";
+		output<<mesh.nodes[v2][0] << "\t" << mesh.nodes[v2][1] << "\n";
+		output<<mesh.nodes[v0][0] << "\t" << mesh.nodes[v0][1] << "\n\n";
 	}
 
 	output.close();
 
 	/* Write GNUplot command */
-	string meshfile_name;
-	meshfile_name = polyname + "_mesh.gnuplot";
+	string gnuplot_filename;
+	gnuplot_filename = filename + "_mesh.gnuplot";
 	ofstream gnuplot_file;
-	gnuplot_file.open(meshfile_name.c_str());
+	gnuplot_file.open(gnuplot_filename.c_str());
 	if (!gnuplot_file.is_open())								// exit if failed to open file
 		{cout << "Failed to open gnuplot file for mesh plot... \n"; exit(1);}
 	gnuplot_file << "set terminal png\n";							// output to "png" file
-	gnuplot_file << "set output \"" << polyname << "_mesh.png\"\n";
+	gnuplot_file << "set output \"" << filename << "_mesh.png\"\n";
 	gnuplot_file << "set size ratio 0.5\n";
 	gnuplot_file << "plot \"" << filename << "\" with lines\n";
 	gnuplot_file.close();
 
 	/* Call gnuplot to make plot */
-//	string cmd = "gnuplot " + meshfile_name + " --persist";
-	string cmd = "gnuplot " + meshfile_name + " \n";
+//	string cmd = "gnuplot " + gnuplot_filename + " --persist";
+	string cmd = "gnuplot " + gnuplot_filename + " \n";
 	system(cmd.c_str());
 }
--- triangle/write_polyfile.cpp
+++ triangle/write_polyfile.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
@@ -10,25 +11,31 @@ using namespace std;
 /*Note: a few assumptions are made for this function 
 /*(1) no node attribute; ("node marker" can exist, though)
 /*(2) no hole.
+/*(3) no maximum area constraint; it would be stored in a ".area" file if needed
 /***********************************************************************************/
 
 int WritePolyfile(string polyname,
-		int num_nodes, double x[], double y[], int num_node_marker, int node_marker[],		// vertices
-		int num_seg, int segments[], int num_seg_attr, int seg_attr[],				// segments
-		int num_region_attr, double region_x[], double region_y[], int region_attr[])		// regional attributes
+		int num_nodes, vector<double> x, vector<double> y, int num_node_marker, vector<int> node_marker,	// vertices
+		int num_seg, vector< vector<int> > segments, int num_seg_attr, vector<int> seg_attr,			// segments
+		int num_region_attr, vector<double> region_x, vector<double> region_y, vector<int> region_attr)	// regional attributes
 {
 	if (num_nodes==0 || num_seg==0)
 		{cout << "Invalid input: vertex/segmenet number have to be non-zero!\n";	return 1; }
+	if (num_node_marker!=0 && num_node_marker!=1)
+		{cout << "num_of_node_marker is " << num_node_marker << "; it has to be either 0 or 1\n";	exit(1);}
+	if (num_seg_attr !=0 && num_seg_attr !=1)
+		{cout << "number of segment attributes is " << num_seg_attr << "; it has to be either 0 or 1\n";	exit(1);}
 
+	/* Open .poly file */
 	polyname += ".poly";								// by default, polyname has no extension
 	ofstream polyfile;
 	polyfile.open(polyname.c_str());
 	if (!polyfile.is_open())
 		{cout << "Failed to open " << polyname << "\n"; return 1;}
 
-	// Write vertices
+	/* 1. Write vertices */
 	polyfile << "#num of vertices is " << num_nodes << "; dimension is 2; " \
-		  << "num of attributes is 0; " << "num of markers is " << num_node_marker << "\n";
+		  << "num of attributes is 0; " << "num of markers is " << num_node_marker << "\n";	// no "attributes" for node
 
 	polyfile << num_nodes << "\t" << 2 << "\t" << 0 << "\t" << num_node_marker << "\n";
 	for (int i=0; i< num_nodes; i++)
@@ -39,11 +46,11 @@ int WritePolyfile(string polyname,
 	}
 	polyfile << "\n\n";
 
-	// Write segments
+	/* 2. Write segments */
 	polyfile << "# " << num_seg << " segments; " << num_seg_attr << " segment attribute\n";
 	polyfile << num_seg << "\t" << num_seg_attr << "\n";
 	for (int i=0; i<num_seg; i++)
-	{	polyfile << i << "\t" << segments[2*i] << "\t" << segments[2*i+1];
+	{	polyfile << i << "\t" << segments[i][0] << "\t" << segments[i][1];
 		if (num_seg_attr == 1)
 			polyfile << "\t" << seg_attr[i];
 		polyfile << "\n";
@@ -59,7 +66,7 @@ int WritePolyfile(string polyname,
 	polyfile << "# " << num_region_attr << " regional attributes\n"; 
 	for (int i=0; i<num_region_attr; i++)
 	{	polyfile << i << "\t" << region_x[i] << "\t" << region_y[i] << "\t"
-			 << region_attr[i] << "\t" << -1 << "\n";
+			 << region_attr[i] << "\t" << -1 << "\n";			// no maximum area constrait (set to "-1")
 	}
 
 	polyfile.close();
