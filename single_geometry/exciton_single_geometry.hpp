3835e77faa5c4515702e429e430580537b4fcebe
xiaoxian <liuxiaox@sas.upenn.edu>
2013-06-04 17:38:51 -0400

    finish defining function and function interpolation Ref: functions.cpp

--- single_geometry/exciton_single_geometry.hpp
+++ single_geometry/exciton_single_geometry.hpp
@@ -1,9 +1,31 @@
 #include <string>
+
+#include <armadillo>
 #include "../triangle/mesh.hpp"
 
 
 #ifndef EXCITON_H
 #define EXCITON_H
+
+/* Mesh */
 my_mesh::MeshData generateMesh(std::string filename);
+
+/* Functions associated with exciton equation */
+double generation_rate(double x, double y);			// exciton generation
+double diffusion_coefficient(double x, double y);		// exciton diffusion coefficient
+double decay_rate(double x, double y);				// exciton decay rate (= 1/ life_time)
+double interface_reaction_rate(double x, double y);		// exciton reaction rate on interface
+
+double uD_left(double x, double y);			// Dirichlet boundary condition on the left boundary
+double uD_right(double x, double y);			// Dirichlet boundary condition on the right boundary
+
+
+/* Linear interpolation of functions */
+arma::vec interpolate_func(const my_mesh::MeshData &mesh, double (*f)(double, double));
+
+/* Linear projection of functions */
+arma::vec project_func(const my_mesh::MeshData &mesh, double (*f)(double, double));
+
+
 
 #endif
--- single_geometry/main.cpp
+++ single_geometry/main.cpp
@@ -1,11 +1,35 @@
+#include <iostream>
 #include <string>
 
-#include "exciton_single_geometry.hpp"
+#include <armadillo>
 
+#include "exciton_single_geometry.hpp"
+#include "../fem_assemble/fem_assemble.hpp"
 
 int main()
 {
 	std::string filename = "smart";
-	generateMesh("smart");
+
+	/* 1. Generate mesh */
+	my_mesh::MeshData mesh = generateMesh("smart");
+
+	/* 2. Form coefficient matrix and RHS vector */
+	/* 2.1 Coefficient matrix */
+	arma::vec a = interpolate_func(mesh, diffusion_coefficient);
+	arma::vec c = interpolate_func(mesh, decay_rate);
+	arma::vec d = interpolate_func(mesh, interface_reaction_rate);
+
+	arma::mat A = linear_fem::assembleMatrixA(mesh, a);
+	arma::mat C = linear_fem::assembleMatrixC(mesh, c);
+	arma::mat D = linear_fem::assembleMatrixD(mesh, d);
+
+	arma::mat M = A + C + D;
+
+	/* 2.2 RHS vector */
+	arma::vec g = project_func(mesh, generation_rate);
+
+	std::cout<<"g is \n" << g << "\n";
+
+
 	return 0;
 }
