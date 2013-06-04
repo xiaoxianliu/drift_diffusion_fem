fb6b4829bc0e8b557d53223aa513163e09dde6b8
xiaoxian <liuxiaox@sas.upenn.edu>
2013-06-04 14:23:31 -0400

    Move test files for "fem_assemble" to a subfolder. Modified "fem_assemble.hpp" and makefile correspondingly.

--- fem_assemble/fem_assemble.hpp
+++ fem_assemble/fem_assemble.hpp
@@ -1,16 +1,17 @@
+#include <string>
 #include <armadillo>
 #include "../triangle/mesh.hpp"
 
 
-#ifndef linear_fem
-#define linear_fem
+#ifndef LINEAR_FEM_H
+#define LINEAR_FEM_H
 
-//namespace linear_fem
-
+namespace linear_fem
+{
 arma::mat assembleMatrixA(const my_mesh::MeshData& mesh, const arma::vec& a);
 arma::mat assembleMatrixC(const my_mesh::MeshData& mesh, const arma::vec& c);
 arma::mat assembleMatrixD(const my_mesh::MeshData& mesh, const arma::vec& d);
-
+}
 
 
 #endif
