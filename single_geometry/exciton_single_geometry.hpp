cab1b1b76e6b57ea313b60f65bbc53c47538a975
xiaoxian <liuxiaox@sas.upenn.edu>
2013-06-04 15:32:39 -0400

    Add 4 files: exciton_single_geometry.hpp generate_mesh.cpp main.cpp makefile Successfully generate mesh within folder of "single_geometry"

---
+++ single_geometry/exciton_single_geometry.hpp
@@ -0,0 +1,9 @@
+#include <string>
+#include "../triangle/mesh.hpp"
+
+
+#ifndef EXCITON_H
+#define EXCITON_H
+my_mesh::MeshData generateMesh(std::string filename);
+
+#endif
---
+++ single_geometry/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+#include "exciton_single_geometry.hpp"
+
+
+int main()
+{
+	std::string filename = "smart";
+	generateMesh("smart");
+	return 0;
+}
