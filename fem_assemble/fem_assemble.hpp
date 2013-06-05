3077826064ccc9ca60004db63d54f2317ab3faf9
xiaoxian <liuxiaox@sas.upenn.edu>
2013-06-05 15:36:13 -0400

    Add a function arma::vec interpolateFunction(mesh, double (*f)(double, double))

--- fem_assemble/fem_assemble.hpp
+++ fem_assemble/fem_assemble.hpp
@@ -11,6 +11,10 @@ namespace linear_fem
 arma::mat assembleMatrixA(const my_mesh::MeshData& mesh, const arma::vec& a);
 arma::mat assembleMatrixC(const my_mesh::MeshData& mesh, const arma::vec& c);
 arma::mat assembleMatrixD(const my_mesh::MeshData& mesh, const arma::vec& d);
+
+// interpolate function at nodal points of given mesh
+arma::vec interpolateFunction(const my_mesh::MeshData& mesh, double (*f)(double, double));	
+
 }
 
 
