bf00ab86355053fa900175e8433b444d977d31b7
xiaoxian <liuxiaox@sas.upenn.edu>
2013-06-16 12:21:57 -0400

    Modify files in "gummel_test/" to use "triangle/generate_mesh.cpp" instead of using local functions to generate mesh

--- fem_assemble/gummel_test/assemble_gummel_matrix.cpp
+++ fem_assemble/gummel_test/assemble_gummel_matrix.cpp
@@ -14,51 +14,21 @@ using namespace arma;
 using namespace my_mesh;
 using namespace linear_fem;
 
+	string filename="assemble_gummel_mat";
 
 	/* 1. Generate mesh */
 	MeshData mesh;
 
-	// initialize "mesh"
-	vector<vector<double> > nodes;
-	vector<int> node_markers;
-	vector<vector<int> > segments;
-	vector<int> segment_markers;
-	vector<vector<double> > regions;
-	vector<int> region_markers;
-
-
+	// interface nodes
+	vector<vector<double> > interface_nodes;
 	vector<double> new_node(2);
 	new_node[0] = 0.0;	new_node[1]=1.0;
-	nodes.push_back(new_node);
+	interface_nodes.push_back(new_node);
 	new_node[0] = 0.0;	new_node[1]=0.0;
-	nodes.push_back(new_node);
+	interface_nodes.push_back(new_node);
 
-
-	newMesh(nodes, node_markers,		// 4 corner vertices are added to "nodes"
-		segments, segment_markers,
-		regions, region_markers);
-
-	string filename = "test_gummel";
-	writePolyfile(filename, nodes, node_markers, segments, segment_markers, regions, region_markers);
-
-	// run "triangle" to generate a coarse mesh
-	string cmd = "/home/xiaoxian/bin/triangle/triangle -qzpeAa1 " + filename + ".poly";
-	system(cmd.c_str());
-
-	string node_file_name= filename + ".1.node";
-	string edge_file_name= filename + ".1.edge";
-	string ele_file_name = filename + ".1.ele";
-
-	ReadNodes(mesh, node_file_name);
-	ReadEdges(mesh, edge_file_name);
-	ReadElements(mesh, ele_file_name);
-
-	ComputeTopology(mesh);
-
-	ComputeEdgeLengths(mesh);
-	ComputeElementAreas(mesh);
-
-	gnuplot_mesh(mesh, filename);
+	// generate mesh
+	mesh = generateMesh(filename, interface_nodes);
 
 
 	/* 2. Generate Gummel matrix and write it to output */
