d304b8513ffef43ddd5125c05e8b3617efd926e0
xiaoxian <liuxiaox@sas.upenn.edu>
2013-06-18 17:21:58 -0400

    Slight modification of "fem_assemble/gummel_test/main.cpp"

--- fem_assemble/gummel_test/main.cpp
+++ fem_assemble/gummel_test/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
 	new_node[0]=0;	new_node[1]=1;	interface_nodes.push_back(new_node);
 	new_node[0]=0;	new_node[1]=0;	interface_nodes.push_back(new_node);
 
-	double max_area=1;
+	double max_area=0.001;
 	MeshData mesh = generateMesh(filename, interface_nodes, max_area);
 
 
