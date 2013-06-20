b4b7aa503d30b72f56535e2b058641a9f7ec5b7c
xiaoxian <liuxiaox@sas.upenn.edu>
2013-06-19 23:34:37 -0400

    Add function computing integration of a piecewise linear function over the whole domain.

--- fem_assemble/fem_assemble.hpp
+++ fem_assemble/fem_assemble.hpp
@@ -8,6 +8,8 @@
 
 namespace linear_fem
 {
+
+// Assemble FEM coefficient matrix for different differential operators (1st order approximation)
 arma::mat assembleMatrixA(const my_mesh::MeshData& mesh, const arma::vec& a);
 arma::mat assembleMatrixC(const my_mesh::MeshData& mesh, const arma::vec& c);
 arma::mat assembleMatrixD(const my_mesh::MeshData& mesh, const arma::vec& d);
@@ -20,6 +22,11 @@ arma::vec interpolateFunction(const my_mesh::MeshData& mesh, double (*f)(double,
 // L2 projection of a given function/vector
 arma::vec L2project_Func(	const my_mesh::MeshData &mesh, double (*f)(double, double) );
 arma::vec L2project_Vec(	const my_mesh::MeshData &mesh, const arma::vec &u);
+
+
+// Functionals
+double integrate_Domain(const my_mesh::MeshData &mesh, const arma::vec u);
+
 }
 
 
