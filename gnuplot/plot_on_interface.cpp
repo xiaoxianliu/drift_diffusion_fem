4eed6e03f00372795e6f43e538566c668b32dc07
xiaoxian <liuxiaox@sas.upenn.edu>
2013-07-03 09:27:51 -0400

    Put "plot on whole mesh" and "plot on interface" in seperate files: 	plot_on_whole_mesh.cpp 	plot_on_interface.cpp

--- gnuplot/plot_on_interface.cpp
+++ gnuplot/plot_on_interface.cpp
@@ -10,61 +10,6 @@
 namespace my_gnuplot
 {
 
-/* Plot arma::vec over the entire domain "Omega" */
-int plot_ArmaVec(const my_mesh::MeshData& mesh, const arma::vec u, const std::string& filename, std::string terminal)
-{using namespace std;
-
-
-	/* Write dat file for solution */
-	ofstream output;
-	string output_name = filename + "_solution.dat";
-	output.open(output_name.c_str());
-	if (!output.is_open())
-		{cout << "Failed to open file " << output_name << "\n"; exit(1);}
-
-	/* Write coordinates and solution at each node; for loop through all elements */
-	for (int t=0; t<mesh.num_elements; t++)
-	{	int v0 = mesh.elements[t][0];
-		int v1 = mesh.elements[t][1];
-		int v2 = mesh.elements[t][2];
-
-		output << mesh.nodes[v0][0] << "\t" << mesh.nodes[v0][1] << "\t" << u(v0) << "\n";
-		output << mesh.nodes[v1][0] << "\t" << mesh.nodes[v1][1] << "\t" << u(v1) << "\n";
-		output << mesh.nodes[v2][0] << "\t" << mesh.nodes[v2][1] << "\t" << u(v2) << "\n";
-		output << mesh.nodes[v0][0] << "\t" << mesh.nodes[v0][1] << "\t" << u(v0) << "\n\n\n";
-	}
-	output.close();
-
-	/* Write GNUplot file */
-	string gnuplot_filename = filename + "_solution.gnuplot";
-	ofstream gnuplot_fstream;
-	gnuplot_fstream.open(gnuplot_filename.c_str());
-	if (!gnuplot_fstream.is_open())
-		{cout<< "Failed to open " << gnuplot_filename << "\n"; exit(1);	}
-
-	if (terminal=="wxt")
-	{	gnuplot_fstream << "set terminal wxt\n"
-				<< "splot \"" << output_name << "\" with lines\n";
-	}
-	else if (terminal == "png")
-	{	gnuplot_fstream	<< "set terminal png\n"
-				<< "set output \"" << filename << "_solution.png\"\n"
-				<< "splot \"" << output_name << "\" with lines\n";
-	}
-	gnuplot_fstream.close();
-
-
-	/* Run GNUplot file */
-	string cmd = "gnuplot " + gnuplot_filename + " --persist\n";
-	system(cmd.c_str());
-
-	return 0;
-}
-
-
-
-
-
 
 /* Plot a arma::vec defined over "Omega" only on the interface "Gamma" */
 int plot_ArmaVec_on_Interface(const my_mesh::MeshData &mesh, const arma::vec &u, std::string filename, std::string terminal)
